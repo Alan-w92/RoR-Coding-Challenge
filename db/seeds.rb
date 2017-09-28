f616b6490caaacf5b61b44b387c311d9c2f9d5a0
Alan W <alanw2129@gmail.com>
2017-09-28 19:22:54 -0400

    api-endpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Blog.create(
+  Id: 1,
+  title: 'Amazon rain puts out forest fire',
+  description: 'The three day unstoppable forest fire has finally been put out by nature.',
+  author: 'Kevin Hinder',
+  created_at: "12.4.15",
+  updated_at: "7.7.16"
+)
+
+Blog.create(
+  Id: 2,
+  title: 'New historic discovery',
+  description: 'A 5000 year old statue of ugera heroa has been found beneath the Sahara Desert.',
+  author: 'Mindy Swechdager',
+  created_at: "4.4.15",
+  updated_at: "8.7.15"
+)
+
+Blog.create(
+  Id: 3,
+  title: 'New cancer medication invented',
+  description: 'Doctor Hayden reporting new medication that can fight back against cancer.',
+  author: 'Tooly Washack',
+  created_at: "1.24.46",
+  updated_at: "3.7.46"
+)
